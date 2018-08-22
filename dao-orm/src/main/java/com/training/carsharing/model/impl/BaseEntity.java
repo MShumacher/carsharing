defa369efec54545a605f139f09d0c95cd03cf45
mshumacher <pavel.aliashkevich@instinctools.ru>
2018-08-22 11:07:44 +0300

    merge

--- dao-orm/src/main/java/com/training/carsharing/model/impl/BaseEntity.java
+++ dao-orm/src/main/java/com/training/carsharing/model/impl/BaseEntity.java
@@ -1,11 +1,8 @@
 package com.training.carsharing.model.impl;
 
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
